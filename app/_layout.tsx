39cb6e7323754a5036c732b1980477fb09ee663c
IMBEKA <imbeka06@gmail.com>
2026-03-12 00:55:55 +0300

    build bottom navigation bar with icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,37 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import { colors } from '../../src/theme/colors';
 
-export default function RootLayout() {
+export default function TabLayout() {
   return (
-    <>
-      <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }}>
-        {/* The index.tsx (Login) will load first automatically */}
-        <Stack.Screen name="index" />
-        {/* We register the tabs folder so we can navigate to it later */}
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </>
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: colors.primary,
+      tabBarInactiveTintColor: colors.textSecondary,
+      headerStyle: { backgroundColor: colors.primary },
+      headerTintColor: colors.white,
+      headerTitleAlign: 'center',
+    }}>
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: 'Home', 
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} /> 
+        }} 
+      />
+      <Tabs.Screen 
+        name="units" 
+        options={{ 
+          title: 'Units & Chats', 
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubbles" size={24} color={color} /> 
+        }} 
+      />
+      <Tabs.Screen 
+        name="notifications" 
+        options={{ 
+          title: 'Notifications', 
+          tabBarIcon: ({ color }) => <Ionicons name="notifications" size={24} color={color} /> 
+        }} 
+      />
+    </Tabs>
   );
 }
