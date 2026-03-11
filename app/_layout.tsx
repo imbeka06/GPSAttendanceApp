da9b09cb5830e122b1dec3828ea048950aa04e18
IMBEKA <imbeka06@gmail.com>
2026-03-12 00:26:23 +0300

    creeated the layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
-export default function Layout() {
+export default function RootLayout() {
   return (
     <>
       <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* The index.tsx (Login) will load first automatically */}
+        <Stack.Screen name="index" />
+        {/* We register the tabs folder so we can navigate to it later */}
+        <Stack.Screen name="(tabs)" />
+      </Stack>
     </>
   );
 }
