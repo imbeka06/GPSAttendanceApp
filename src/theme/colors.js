77f54c7048eba527416d990115a1cf645b4eec0a
IMBEKA <imbeka06@gmail.com>
2026-03-12 01:44:56 +0300

    fixed green color in color.js

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -5,7 +5,8 @@ export const colors = {
   white: '#FFFFFF',
   textPrimary: '#1E5631',
   textSecondary: '#666666',
-  success: '#28a745', // <-- We added this missing color!
+  success: '#28a745', 
+  error: '#dc3545', // <-- Added the missing red color here!
 };
 
 export const shadowStyle = {
