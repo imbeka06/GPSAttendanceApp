6622c5a0ffb9ba75a37163f7895b71b85900fc12
IMBEKA <imbeka06@gmail.com>
2026-04-26 13:07:58 +0300

    Wrapped root layout with AttendanceProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { AttendanceProvider } from '../src/context/AttendanceContext';
 
 export default function RootLayout() {
   return (
-    <>
+    <AttendanceProvider>
       <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="(tabs)" />
-        {/* ADDED THIS LINE FOR THE LECTURER FLOW */}
         <Stack.Screen name="(lecturer)" />
       </Stack>
-    </>
+    </AttendanceProvider>
   );
 }
