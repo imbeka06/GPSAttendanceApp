1453863025e1e6b1d48bbde906ebc38bca911e15
IMBEKA <imbeka06@gmail.com>
2026-04-27 13:24:44 +0300

    On lecturere: features that, register units, create-unit, and unit-materials screens in lecturer stack navigator

--- app/(lecturer)/_layout.tsx
+++ app/(lecturer)/_layout.tsx
@@ -6,7 +6,12 @@ export default function LecturerLayout() {
   return (
     <>
       <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="units" />
+        <Stack.Screen name="create-unit" />
+        <Stack.Screen name="unit-materials" />
+      </Stack>
     </>
   );
 }
